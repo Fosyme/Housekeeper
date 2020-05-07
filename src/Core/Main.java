fdc71223cb34c98f4e80bba4f56b972f128f79a9
Fosyme <Fosyme@qq.com>
2020-05-07 23:13:19 +0800

    Start.

---
+++ src/Core/Main.java
@@ -0,0 +1,7 @@
+package Core;
+
+public class Main {
+    public static void main(String[] args) {
+        
+    }
+}
---
+++ src/Dao/Main.java
@@ -0,0 +1,7 @@
+package Dao;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
