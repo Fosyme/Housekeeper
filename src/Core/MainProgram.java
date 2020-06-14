22828156c916de856305f7fd8705ff9ea0a09a18
Fosyme <Fosyme@qq.com>
2020-06-14 17:48:14 +0800

    20200614-xsj-01

--- src/Core/MainProgram.java
+++ src/Core/MainProgram.java
@@ -1,16 +1,16 @@
-//package Core;
-//
-///**
-// * 主程序, 用于操作主界面
-// * @author Fosyme
-// * @version 1.0.0
-// * */
-//public class MainProgram {
-//    private User user;
-//
-//    public MainProgram(User user) {
-//        this.user = user;
-//    }
-//
-//
-//}
+package Core;
+
+/**
+ * 主程序, 用于操作主界面
+ * @author Fosyme
+ * @version 1.0.0
+ * */
+public class MainProgram {
+    private User user;
+
+    public MainProgram(User user) {
+        this.user = user;
+    }
+
+
+}
